80fd33b2b2365e24719d8c1b4162feb909ea6758
walterlimtrajano <128225888+walterlimtrajano@users.noreply.github.com>
2025-05-17 14:34:16 -0300

    primeiro commit

--- projeto/mobile-navbar.js
+++ projeto/mobile-navbar.js
@@ -1,5 +1,5 @@
-class MobileNavbar{
-    constructor(mobileMenu, navList, navLinks){
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
@@ -8,37 +8,36 @@ class MobileNavbar{
         this.handleClick = this.handleClick.bind(this);
     }
 
-animateLinks (){
-    this.navLinks.forEach((link, index) => {
-        link.style.animation
-        ? (link.style.animation = '')
-        : (link.style.animation = 'navLinkFade 0.5s ease forwards 0.3s');
-    });
-}
-
-handleClick() {
-    console.log(this);
-    this.navList.classList.toggle(this.activeClass);
-    this.mobileMenu.classList.toggle(this.activeClass);
-    this.animateLinks();
-}
-
-addClickEvent() {
-    this.mobileMenu.addEventListener('click', this.handleClick);
-}
-
-init() {
-    if (this.mobileMenu){
-        this.addClickEvent();
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = '')
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
     }
-    return this;
-}
 
+    handleClick() {
+        console.log(this);
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener('click', this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
 }
 
 const mobileNavbar = new MobileNavbar(
-'.mobile-menu',
-'.nav-list',
-'.nav-list li',
+    '.mobile-menu',
+    '.nav-list',
+    '.nav-list li'
 );
 mobileNavbar.init();
